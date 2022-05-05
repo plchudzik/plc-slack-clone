5b3ad404d3afe687e13b5d4962d11fc9ccdab0ad
P Chudzik <plchudzik@gmail.com>
2022-05-05 12:45:35 +0100

    header start MUI installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1> Slack clone</h1>
+      {/* HEADER */}
+      {/* SIDEBAR */}
+      {/* REACT-ROUTER -> chat screen*/}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* avatar */}
+        {/* time icon */}
+      </div>
+      <div className="header__search">
+        {/* search icon */}
+        {/* input */}
+      </div>
+      <div className="header__right">{/* help icon */}</div>
+    </div>
+  );
+};
+
+export default Header;
